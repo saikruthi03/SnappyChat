a11d4a4f471811b29e7a271ad5654d7b0ffd1964
saikruthi03 <vsaikruthi92@gmail.com>
2016-12-07 15:16:50 -0800

    Added search and friends option

--- app/src/main/java/com/example/vsaik/snapchat/TimeLineActivity.java
+++ app/src/main/java/com/example/vsaik/snapchat/TimeLineActivity.java
@@ -4,17 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TimeLineActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class TimeLineActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
 
     private ListView listTimeLine = null;
     private Context context= null;
@@ -34,6 +38,16 @@ public class TimeLineActivity extends AppCompatActivity implements AdapterView.O
                 startActivity(main);
             }
         });
+        Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.options_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setSelection(0);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        TextView txtView = (TextView) findViewById(R.id.txtLink);
+        txtView.setText(Html.fromHtml("<a href='#'>"+UserDetails.getNickname().toUpperCase()+"</a>"));
+
         onStart();
     }
 
@@ -87,5 +101,19 @@ public class TimeLineActivity extends AppCompatActivity implements AdapterView.O
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Log.d("PAUSE","Pausing");
     }
-}
 
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+     if(parent.getItemAtPosition(pos).toString().equals("Search")){
+         Intent mainScreen = new Intent(TimeLineActivity.this,SearchActivity.class);
+         startActivity(mainScreen);
+    }else if(parent.getItemAtPosition(pos).toString().equals("Friends")){
+         Intent mainScreen = new Intent(TimeLineActivity.this,FriendActivity.class);
+         startActivity(mainScreen);
+    }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+}
