0ad76dfe088fbb3fc525dc516347bacfb55f025d
saikruthi03 <vsaikruthi92@gmail.com>
2016-12-07 15:07:24 -0800

    Service to send push notifications

--- app/src/main/java/com/example/vsaik/snapchat/GetMessagesService.java
+++ app/src/main/java/com/example/vsaik/snapchat/GetMessagesService.java
@@ -1,15 +1,22 @@
 package com.example.vsaik.snapchat;
 
+import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Process;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,118 +27,82 @@ import java.util.TimerTask;
  */
 public class GetMessagesService extends Service {
 
+        private Looper mServiceLooper;
+        private ServiceHandler mServiceHandler;
 
-private int mId = 12;
-
-
-Context context= null;
-    public int counter=0;
-    public GetMessagesService(Context applicationContext) {
-        super();
-        context = applicationContext;
-      //onCreate();
-        Log.i("HERE", "here I am!");
-
-    }
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.d("In background service","Helo");
-        int count = 1;
-        while(count == 1) {
-            Log.d("In background service","Inside if");
-            NotificationCompat.Builder mBuilder =
-                    new NotificationCompat.Builder(context)
-                            .setSmallIcon(R.drawable.com_facebook_button_icon)
-                            .setContentTitle("My notification")
-                            .setContentText("Hello World!");
-// Creates an explicit intent for an Activity in your app
-            Intent resultIntent = new Intent(context, MainScreen.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-// Adds the back stack for the Intent (but not the Intent itself)
-            stackBuilder.addParentStack(MainScreen.class);
-// Adds the Intent that starts the Activity to the top of the stack
-            stackBuilder.addNextIntent(resultIntent);
-            PendingIntent resultPendingIntent =
-                    stackBuilder.getPendingIntent(
-                            0,
-                            PendingIntent.FLAG_UPDATE_CURRENT
-                    );
-            mBuilder.setContentIntent(resultPendingIntent);
-            NotificationManager mNotificationManager =
-                    (NotificationManager) getSystemService(context.NOTIFICATION_SERVICE);
-            // mId allows you to update the notification later on.
-            mNotificationManager.notify(mId, mBuilder.build());
-            count++;
-        }
-        // here you can add whatever you want this service to do
-    }
-
-    public GetMessagesService() {
-    }
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
-        onCreate();
-        startTimer();
-        return START_STICKY;
-    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.i("EXIT", "ondestroy!");
-        Intent broadcastIntent = new Intent("RestartService");
-        sendBroadcast(broadcastIntent);
-        stoptimertask();
-    }
-
-    private Timer timer;
-    private TimerTask timerTask;
-    long oldTime=0;
-    public void startTimer() {
-        //set a new Timer
-        timer = new Timer();
-
-        //initialize the TimerTask's job
-        initializeTimerTask();
-
-        //schedule the timer, to wake up every 1 second
-        timer.schedule(timerTask, 1000, 1000); //
-    }
-
-    /**
-     * it sets the timer to print the counter every x seconds
-     */
-    public void initializeTimerTask() {
-        timerTask = new TimerTask() {
-            public void run() {
-                Log.i("in timer", "in timer ++++  "+ (counter++));
+        // Handler that receives messages from the thread
+        private final class ServiceHandler extends Handler {
+            public ServiceHandler(Looper looper) {
+                super(looper);
             }
-        };
-    }
-
-    /**
-     * not needed
-     */
-    public void stoptimertask() {
-        //stop the timer, if it's not already null
-        if (timer != null) {
-            timer.cancel();
-            timer = null;
+            @Override
+            public void handleMessage(Message msg) {
+                notification();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    // Restore interrupt status.
+                    Thread.currentThread().interrupt();
+                }
+                // Stop the service using the startId, so that we don't stop
+                // the service in the middle of handling another job
+                stopSelf(msg.arg1);
+            }
         }
+
+        @Override
+        public void onCreate() {
+            HandlerThread thread = new HandlerThread("ServiceStartArguments",
+                    Process.THREAD_PRIORITY_BACKGROUND);
+            thread.start();
+
+            // Get the HandlerThread's Looper and use it for our Handler
+            mServiceLooper = thread.getLooper();
+            mServiceHandler = new ServiceHandler(mServiceLooper);
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+
+            Message msg = mServiceHandler.obtainMessage();
+            msg.arg1 = startId;
+            mServiceHandler.sendMessage(msg);
+
+            return START_STICKY;
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            // We don't provide binding, so return null
+            return null;
+        }
+
+        @Override
+        public void onDestroy() {
+            Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+        }
+        public void notification(){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(R.drawable.notify)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+        Intent resultIntent = new Intent(getApplicationContext(), ChatActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+        stackBuilder.addParentStack(ChatActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(123, mBuilder.build());
     }
 
-
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
 }
+
 
