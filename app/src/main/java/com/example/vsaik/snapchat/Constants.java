981bc0b7f83ceaff8dccef85a4b2d7dd6d9b4a1e
saikruthi03 <vsaikruthi92@gmail.com>
2016-12-07 15:08:21 -0800

    Added firebase constants

--- app/src/main/java/com/example/vsaik/snapchat/Constants.java
+++ app/src/main/java/com/example/vsaik/snapchat/Constants.java
@@ -1,5 +1,7 @@
 package com.example.vsaik.snapchat;
 
+
+
 /**
  * Created by jay on 12/6/16.
  */
@@ -7,4 +9,9 @@ package com.example.vsaik.snapchat;
 public class Constants {
 
     public final static String URL = "http://ec2-35-164-97-85.us-west-2.compute.amazonaws.com:8080";
+    public final static String dataBase = "users";
+    public final static String Private = "Private";
+    public final static String Public = "Public";
+    public final static String Friends = "FriendsOnly";
 }
+
