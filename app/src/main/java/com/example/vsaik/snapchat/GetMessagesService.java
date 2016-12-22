997225975c71b5ebee1a46ee0396b990b647e59f
saikruthi03 <vsaikruthi92@gmail.com>
2016-12-21 23:22:03 -0800

    Push notifications update

--- app/src/main/java/com/example/vsaik/snapchat/GetMessagesService.java
+++ app/src/main/java/com/example/vsaik/snapchat/GetMessagesService.java
@@ -7,6 +7,7 @@ import android.app.Service;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
@@ -18,6 +19,12 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,67 +34,101 @@ import java.util.TimerTask;
  */
 public class GetMessagesService extends Service {
 
-        private Looper mServiceLooper;
-        private ServiceHandler mServiceHandler;
+    public int uniqueNumber = 0;
+    public HashMap<String,String> liveData = new HashMap<>();
+    final static String GROUP_KEY_EMAILS = "snappy_chat";
+    JSONArray responseFetch = null;
+    List<String> type = new ArrayList<>();
+    List<String> data = new ArrayList<>();
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-        // Handler that receives messages from the thread
-        private final class ServiceHandler extends Handler {
-            public ServiceHandler(Looper looper) {
-                super(looper);
-            }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask doAsynchronousTask = new TimerTask() {
             @Override
-            public void handleMessage(Message msg) {
-                notification();
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    // Restore interrupt status.
-                    Thread.currentThread().interrupt();
-                }
-                // Stop the service using the startId, so that we don't stop
-                // the service in the middle of handling another job
-                stopSelf(msg.arg1);
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                           new DoBackgroundTask().execute();
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                        }
+                    }
+                });
             }
-        }
-
-        @Override
-        public void onCreate() {
-            HandlerThread thread = new HandlerThread("ServiceStartArguments",
-                    Process.THREAD_PRIORITY_BACKGROUND);
-            thread.start();
-
-            // Get the HandlerThread's Looper and use it for our Handler
-            mServiceLooper = thread.getLooper();
-            mServiceHandler = new ServiceHandler(mServiceLooper);
-        }
-
-        @Override
-        public int onStartCommand(Intent intent, int flags, int startId) {
-            Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-
-            Message msg = mServiceHandler.obtainMessage();
-            msg.arg1 = startId;
-            mServiceHandler.sendMessage(msg);
+        };
+        timer.schedule(doAsynchronousTask, 0, 5000); //execute in every 50000 ms
 
             return START_STICKY;
-        }
+
+    }
+    private class DoBackgroundTask extends AsyncTask<Void, Void, Void> {
 
         @Override
-        public IBinder onBind(Intent intent) {
-            // We don't provide binding, so return null
+        protected Void doInBackground(Void... params) {
+            String response = "";
+           data= new ArrayList<>();
+            type= new ArrayList<>();
+            liveData = new HashMap<>();
+            liveData.put("Method", "GET");
+            liveData.put("URL", Constants.URL + "/get_users_live");
+            liveData.put("username", UserDetails.getUserName());
+            GetData fetch3 = new GetData(liveData);
+            try {
+                response = fetch3.doInBackground();
+                Log.e("RESPONSE- ERROR-get", response);
+            } catch (Exception e) {
+                Log.e("RESPONSE- ERROR-get", e.toString());
+            }
+            int size = response.length();
+            try{  if(size>0){
+                responseFetch = new JSONArray(response);
+                size = responseFetch.length();
+                for (int i = 0; i < size; i++) {
+                    try {
+                        JSONObject object = responseFetch.getJSONObject(i);
+                        data.add(object.getString("data"));
+                        type.add(object.getString("type"));
+                    }catch(Exception ex){
+
+                    }
+
+                }
+
+            }}catch(Exception ex){
+
+            }
             return null;
         }
 
         @Override
-        public void onDestroy() {
-            Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+        protected void onPostExecute(Void aVoid) {
+if(data.size() > 0 && type.size() > 0){
+    for(int i=0;i<data.size();i++){
+        notification(data.get(i),type.get(i));
+    }
+}
         }
-        public void notification(){
+
+    }
+
+    public void notification(String data,String type){
+        uniqueNumber++;
+        Toast.makeText(getApplicationContext(),"inside app context", Toast.LENGTH_LONG).show();
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(getApplicationContext())
                         .setSmallIcon(R.drawable.notify)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!");
+                        .setContentTitle(type.toUpperCase())
+                        .setContentText(data)
+                        .setGroup(GROUP_KEY_EMAILS);
+
         Intent resultIntent = new Intent(getApplicationContext(), ChatActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
         stackBuilder.addParentStack(ChatActivity.class);
@@ -100,9 +141,11 @@ public class GetMessagesService extends Service {
         mBuilder.setContentIntent(resultPendingIntent);
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        mNotificationManager.notify(123, mBuilder.build());
+        mNotificationManager.notify(uniqueNumber, mBuilder.build());
     }
 
 }
+
+
 
 
